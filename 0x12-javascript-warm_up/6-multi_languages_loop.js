0fd58620af5fc6305b48817c304074d3963b93dc
user name <user email>
2023-04-11 04:29:56 -0700

    Fix code error

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
-const lang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const i of lang) {
-  console.log(i);
+const myLines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const arrayLength = myLines.length;
+let i = 0;
+while (i < arrayLength) {
+  console.log(myLines[i]);
+  i++;
 }
