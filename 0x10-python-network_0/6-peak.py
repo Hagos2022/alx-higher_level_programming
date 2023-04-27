c0825239eaf11b68932051b21bc7479fa28bd004
user name <hagos69@gmail.com>
2023-04-27 07:57:05 -0700

    Write a function that finds a peak in a list of unsorted integers

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,22 +2,40 @@
 """Defines a peak-finding algorithm /function"""
 
 
-def find_peak(list_of_integers):
-    """Function that returns a peak in a list of unsorted integers."""
-    if list_of_integers == []:
+def peak_recur(a, Min, Max):
+    """ Recursion using Binary Search """
+
+    if not a:
         return None
 
-    size = len(list_of_integers)
-    if size == 1:
-        return list_of_integers[0]
-    elif size == 2:
-        return max(list_of_integers)
+    half = int((Max + Min) / 2)
 
-    mid = int(size / 2)
-    peak = list_of_integers[mid]
-    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
-        return peak
-    elif peak < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
-    else:
-        return find_peak(list_of_integers[mid + 1:])
+    if (half - 1) < Min and (half + 1) > Max:
+        return a[half]
+
+    if (half - 1) < Min and (half + 1) <= Max and a[half + 1] <= a[half]:
+        return a[half]
+
+    if (half + 1) > Max and (half - 1) >= Min and a[half - 1] <= a[half]:
+        return a[half]
+
+    if (half - 1) >= Min and a[half - 1] <= a[half] \
+       and (half + 1) <= Max and a[half + 1] <= a[half]:
+        return a[half]
+
+    if a[half + 1] > a[half]:
+        return peak_recur(a, half + 1, Max)
+
+    return peak_recur(a, 0, half - 1)
+
+
+def find_peak(list_of_integers):
+    """ A function that finds a peak in a list of unsorted integers """
+
+    if not list_of_integers:
+        return None
+
+    Min = 0
+    Max = len(list_of_integers) - 1
+
+    return peak_recur(list_of_integers, Min, Max)
