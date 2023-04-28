631ec1824f714eaf0fca6e18e889cf80f53ce170
user name <hagos69@gmail.com>
2023-04-28 05:43:04 -0700

    Check my status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,6 +5,7 @@ import requests
 
 if __name__ == "__main__":
     r = requests.get("https://intranet.hbtn.io/status")
+    body = r.text
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
